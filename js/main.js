57b6de3c0182860195d5b13e4263443ddf528054
khamidullo-sheikh <xmirzaev@list.ru>
2021-09-27 21:19:31 +0300

    making some improvements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+let body = document.querySelector(".wrapper");
 let navMenu = document.querySelector(".header");
 let btnOpen = document.querySelector(".nav__button--toggle");
 let btnClose = document.querySelector(".nav__menu--toggle");
 btnOpen.addEventListener("click", function(){
     navMenu.classList.add("nav__toggle");
+    body.classList.add("lock");
 });
 
 btnClose.addEventListener("click", function(){
     navMenu.classList.remove("nav__toggle");
+    body.classList.remove("lock");
 });
